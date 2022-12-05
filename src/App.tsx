ea30dafa76e427cedd183bb38fcc8746620bf118
Emerson Araujo <emersonsantos@gmail.com>
2022-12-04 21:01:42 -0300

    feat: adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter} from "react-router-dom";
+import { AppRoutes } from './routes';
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <AppRoutes/>
+      </div>
+      </BrowserRouter>
   );
 }
 
